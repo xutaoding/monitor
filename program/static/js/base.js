1ae9b2bad3e0ef6cdfc41d2d725c08d1baf67b5d
xutaoding <xutaoding@163.com>
2015-11-13 08:17:59 +0800

    add some function about modal

--- program/static/js/base.js
+++ program/static/js/base.js
@@ -1,7 +1,20 @@
 ﻿$(function(){
     $('[data-toggle="modal"]').click(function(){
+        var records = [];
         var data_id = $(this).attr('data-id');
-        alert($('[data-id="' + data_id +'"]').html());
+        console.log(data_id );
+        console.log('[data-id="' + data_id + '"] td');
+
+        $('[data-id="' + data_id + '"] td').each(function(i){
+            var value = $(this).attr('class');
+
+            if (value != "extra"){
+                records.push($(this).text());
+                console.log($(this).text());
+            }
+        });
+
+//        alert(records);
     });
 
 
