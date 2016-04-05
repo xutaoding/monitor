6a685f94c55f715190d6285112f9d893931706b2
xutaoding <xutaoding@163.com>
2016-04-05 23:23:53 +0800

    fix bugs

--- monitor/urls.py
+++ monitor/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from eggs.controllers.auth.login import Login
+from eggs.controllers.auth import login
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', Login.as_view(), name='login'),
+    url(r'^login/$', login, name='login'),
     url(r'^program/', include('program.urls', namespace='program')),
 ]
